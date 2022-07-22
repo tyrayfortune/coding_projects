20989f06554fa5d2226a66e46778dc6b97c04e98
tyrayfortune <99374142+tyrayfortune@users.noreply.github.com>
2022-07-22 10:59:26 -0700

    regex update

--- practice/practice.js
+++ practice/practice.js
@@ -15,21 +15,28 @@
 // console.log(array[2])
 
 
-const result = {
-    success: ["max-length", "no-amd", "prefer-arrow-functions"],
-    failure: ["no-var", "var-on-top", "linebreak"],
-    skipped: ["no-extra-semi", "no-dup-keys"]
-  };
-  function makeList(arr) {
-    // Only change code below this line
-    const failureItems = [];
-    for(let i = 0; i < arr.length ; i++){
-        failureItems.push(` '<li class="text-warning">${arr[i]}</li>'`)
-    }
-    // Only change code above this line
+// const result = {
+//     success: ["max-length", "no-amd", "prefer-arrow-functions"],
+//     failure: ["no-var", "var-on-top", "linebreak"],
+//     skipped: ["no-extra-semi", "no-dup-keys"]
+//   };
+//   function makeList(arr) {
+//     // Only change code below this line
+//     const failureItems = [];
+//     for(let i = 0; i < arr.length ; i++){
+//         failureItems.push(` '<li class="text-warning">${arr[i]}</li>'`)
+//     }
+//     // Only change code above this line
   
-    return failureItems;
-  }
+//     return failureItems;
+//   }
   
-  const failuresList = makeList(result.failure);
-  console.log(failuresList)
+//   const failuresList = makeList(result.failure);
+//   console.log(failuresList)
+
+
+  // function addition (add, me)  {
+  //   return add + me
+  // }
+
+  // console.log(addition(2,4))
