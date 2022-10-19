1ccb9288a160297d442406f2fe9816b06e9945b7
tyrayfortune <99374142+tyrayfortune@users.noreply.github.com>
2022-10-18 19:13:50 -0700

    first commit for bank app

--- MERN Projects/chat_app/client/src/App.js
+++ MERN Projects/chat_app/client/src/App.js
@@ -3,13 +3,16 @@ import Join from './components/Join'
 import Chat from './components/Chat'
 import { BrowserRouter, BrowserRouter as Routers, Route} from "react-router-dom"
 
+
 const App = () => (    
     <BrowserRouter>
         <Routers>
             {/* where user gets directed, to our join component */}
-            <Route path="/" exact component={join}/>
+            <Route path="/" exact component={Join}/>
             {/* when user puits ion required info to join, we send to chat */}
-            <Route path="/chat"  component={chat}/>
+            <Route path="/chat"  component={Chat}/>
         </Routers>
     </BrowserRouter>
 )
+
+export default App
