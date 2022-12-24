c2b803e885b5d3f1d35001585777c35db7c07eb7
tyrayfortune <99374142+tyrayfortune@users.noreply.github.com>
2022-12-23 16:17:44 -0800

    started NoCap project

--- MERN Projects/NoCap/client/src/App.jsx
+++ MERN Projects/NoCap/client/src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './App.css';
+import{BrowserRouter, Routes, Route} from "react-router-dom"
+import Homepage from "./components/Homepage";
+import React, {useState} from 'react'
 
-const App = () => {
+function App() {
+//STATE FOR KEEPING TRACK OF REFRESH
+const [refreshState, setRefreshState] = useState(false)
+
+//FUNCTION FOR KEEPING TRACK OF REFRESH
+const refresh = () => {
+  setRefreshState(!refreshState)
+}
   return (
-    <div className='text-3xl font-bold underline'>Hello world</div>
-  )
+    <BrowserRouter>
+    <div >
+      <Routes>
+      {/* MAKE SURE TO PUT THE REFRESH INSIDE THE ELEMENT!!!!!!! NOT OUTSIDE THE BRACES */}
+      <Route path="/" element={<Homepage refresh={refresh} refreshState={refreshState}/> } />
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ MERN Projects/NoCap/client/src/components/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from './Button'
+
+const Homepage = () => {
+
+    return (
+        <div>
+            <h1>Welcome to NoCap, <br />where you can view and correct any mistaken punctiation. </h1>
+            <form action="">
+                <label htmlFor="">input text here</label>
+                <input type="text" name="" id="" />
+                <h3></h3>
+                <Button />
+            </form>
+        </div>
+    )
+}
+
+export default Homepage
